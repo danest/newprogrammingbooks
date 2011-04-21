2488a92ca95029f860d0a1c930432d186715489c
Kevin Urrutia <urrutia.kevin@gmail.com>
2011-04-20 23:19:01 -0400

    session fixing

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,9 @@ class BooksController < ApplicationController
   end
   
   def create
-    session[:fb_auth] = request.env[:omniauth.auth]
-    session[:fb_token] = session[:fb_auth][:credentials][:token]
-    session[:fb_error] = nil
+    session['fb_auth'] = request.env['omniauth.auth']
+    session['fb_token'] = session['fb_auth']['credentials']['token']
+    session['fb_error'] = nil
     redirect_to root_path
   end
   
@@ -21,7 +21,7 @@ class BooksController < ApplicationController
   
   def failure
     clear_session
-    session[:fb_error] = 'In order to use this site you must allow us access to your Facebook data<br />'
+    session['fb_error'] = 'In order to use this site you must allow us access to your Facebook data<br />'
     redirect_to root_path
   end
   
@@ -34,9 +34,9 @@ class BooksController < ApplicationController
   end
   
   def clear_session
-    session[:fb_auth] = nil
-    session[:fb_token] = nil
-    session[:fb_error] = nil
+    session['fb_auth'] = nil
+    session['fb_token'] = nil
+    session['fb_error'] = nil
   end
   
 end
