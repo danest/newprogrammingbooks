60042e1e9929e96d8605cc28a7488f13bd03e367
Kevin Urrutia <urrutia.kevin@gmail.com>
2011-04-22 18:29:43 -0400

    added redirect

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
     session['fb_token'] = session['fb_auth']['credentials']['token']
     session['fb_error'] = nil
     cookies.permanent.signed[:remember_token] = "79ee2f56999b916cf5043a12e9cbcb98"
+    redirect_to root_path
   end
   
   def destroy
