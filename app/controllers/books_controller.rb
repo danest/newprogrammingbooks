f53c338824b9eee2ddf4271d33e90a7f652656b1
Kevin Urrutia <urrutia.kevin@gmail.com>
2011-04-22 13:40:45 -0400

    changed sessions to cookies

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,15 +3,12 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.find(:all, :order => "id DESC")
-    # if session[:fb_auth]
-    #   redirect_to terms_path
-    # end
   end
   
   def create
-    session['fb_auth'] = request.env['omniauth.auth']
-    session['fb_token'] = session['fb_auth']['credentials']['token']
-    session['fb_error'] = nil
+    cookies.permanent.signed['fb_auth'] = request.env['omniauth.auth']
+    cookies.permanent.signed['fb_token'] = session['fb_auth']['credentials']['token']
+    cookies.permanent.signed['fb_error'] = nil
     redirect_to root_path
   end
   
@@ -22,7 +19,7 @@ class BooksController < ApplicationController
   
   def failure
     clear_session
-    session['fb_error'] = 'In order to use this site you must allow us access to your Facebook data<br />'
+    cookies.permanent.signed['fb_error'] = 'In order to use this site you must allow us access to your Facebook data<br />'
     redirect_to root_path
   end
   
