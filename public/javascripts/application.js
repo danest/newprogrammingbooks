7aae140cd197bd5f6d0a26a50e760c0c9b63daba
Kevin Urrutia <urrutia.kevin@gmail.com>
2011-04-22 18:34:10 -0400

    put back js in file

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,21 +1,21 @@
-// $(document).ready(function () {
-// 
-// 	function popupCenter(url, width, height, name) {
-// 	  var left = (screen.width/2)-(width/2);
-// 	  var top = (screen.height/2)-(height/2);
-// 	  //return window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
-// 	  //this is for the url to work in canvas
-// 	  return window.top.location.replace(url);
-// 	}
-// 
-// 	$("a.popup").click(function(e) {
-// 	  popupCenter($(this).attr("href"), $(this).attr("data-width"), $(this).attr("data-height"), "authPopup");
-// 	  e.stopPropagation(); return false;
-// 	});
-// 	
-// 	// if(window.opener) {
-// 	//     window.opener.location.reload(true);
-// 	//     window.close()
-// 	//   }
-// 
-// });
+$(document).ready(function () {
+
+	function popupCenter(url, width, height, name) {
+	  var left = (screen.width/2)-(width/2);
+	  var top = (screen.height/2)-(height/2);
+	  //return window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
+	  //this is for the url to work in canvas
+	  return window.top.location.replace(url);
+	}
+
+	$("a.popup").click(function(e) {
+	  popupCenter($(this).attr("href"), $(this).attr("data-width"), $(this).attr("data-height"), "authPopup");
+	  e.stopPropagation(); return false;
+	});
+	
+	// if(window.opener) {
+	//     window.opener.location.reload(true);
+	//     window.close()
+	//   }
+
+});
